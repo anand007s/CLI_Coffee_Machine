7b119681112ae3ee26dfd51e241e275ed8bbd74e
Anand <hianandhowru@gmail.com>
2023-09-28 13:16:54 +0530

    Keep track of the supplies

--- machine/AskUser.java
+++ machine/AskUser.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class AskUser {
 
     static String action;
-    static int buy;
+    static String buy;
     private static void askHowManyCupsOfCoffeeWillNeeded() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Write how many cups of coffee you will need:");
@@ -14,19 +14,19 @@ public class AskUser {
 
     private static void askHowManyMlOfWaterMachineHas() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Write how many ml of water the coffee machine has:");
+        System.out.println("Write how many ml of water you want to add: ");
         RequiredIngrediant.totalMlOfWater += scanner.nextInt();
     }
 
     private static void askHowManyMlOfMilkMachineHas() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Write how many ml of milk the coffee machine has:");
+        System.out.println("Write how many ml of milk you want to add: ");
         RequiredIngrediant.totalMlOfMilk += scanner.nextInt();
     }
 
     private static void askHowManyGmOfCoffeeMachineHas() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        System.out.println("Write how many grams of coffee beans you want to add: ");
         RequiredIngrediant.totalGmOfCoffee += scanner.nextInt();
     }
 
@@ -38,22 +38,31 @@ public class AskUser {
 
     static void writeAction() {
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Write action (buy, fill, take): \n");
-        action = scanner.next();
+        System.out.printf("Write action (buy, fill, take, remaining, exit): \n");
+        action = scanner.nextLine();
     }
 
     static void perfromAction() {
-        if (action.equals("buy")) {
-            performBuyAction();
-        } else if (action.equals("fill")) {
-            perfromFillAction();
-        } else if (action.equals("take")) {
-            perfromTakeAction();
+
+        while (!action.equals("exit")) {
+            if (action.equals("buy")) {
+                performBuyAction();
+                writeAction();
+            } else if (action.equals("fill")) {
+                perfromFillAction();
+                writeAction();
+            } else if (action.equals("take")) {
+                perfromTakeAction();
+                writeAction();
+            } else if (action.equals("remaining")) {
+                RequiredIngrediant.printIntialCapicityOfMachine();
+                writeAction();
+            }
         }
     }
 
     private static void perfromTakeAction() {
-        System.out.printf("I gave you $%d", RequiredIngrediant.totalMoney);
+        System.out.printf("I gave you $%d\n", RequiredIngrediant.totalMoney);
         RequiredIngrediant.totalMoney = 0;
     }
 
@@ -66,29 +75,75 @@ public class AskUser {
 
     private static void performBuyAction() {
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: \n");
-        buy = scanner.nextInt();
+        System.out.printf("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:\n");
+        buy = scanner.next();
         System.out.println();
         switch (buy) {
-            case 1 :
-                RequiredIngrediant.totalMlOfWater -= 250;
-                RequiredIngrediant.totalGmOfCoffee -= 16;
-                RequiredIngrediant.totalCups -= 1;
-                RequiredIngrediant.totalMoney += 4;
+            case "1" :
+                if (RequiredIngrediant.totalMlOfWater >= 250 && RequiredIngrediant.totalGmOfCoffee >= 16 && RequiredIngrediant.totalCups >= 1) {
+                    RequiredIngrediant.totalMlOfWater -= 250;
+                    RequiredIngrediant.totalGmOfCoffee -= 16;
+                    RequiredIngrediant.totalCups -= 1;
+                    RequiredIngrediant.totalMoney += 4;
+                    System.out.printf("I have enough resources, making you a coffee!\n");
+                } else {
+                    String shortResource = null;
+                    if (RequiredIngrediant.totalMlOfWater < 250) {
+                        shortResource = "water";
+                    } else if (RequiredIngrediant.totalGmOfCoffee < 16) {
+                        shortResource = "coffee";
+                    } else if (RequiredIngrediant.totalCups < 1) {
+                        shortResource = "cups";
+                    }
+                    System.out.printf("Sorry, not enough %s!", shortResource);
+                }
                 break;
-            case 2 :
-                RequiredIngrediant.totalMlOfWater -= 350;
-                RequiredIngrediant.totalMlOfMilk -= 75;
-                RequiredIngrediant.totalGmOfCoffee -= 20;
-                RequiredIngrediant.totalCups -= 1;
-                RequiredIngrediant.totalMoney += 7;
+            case "2" :
+                if (RequiredIngrediant.totalMlOfWater >= 350 && RequiredIngrediant.totalMlOfMilk >= 75 && RequiredIngrediant.totalGmOfCoffee >= 20 && RequiredIngrediant.totalCups >= 1) {
+                    RequiredIngrediant.totalMlOfWater -= 350;
+                    RequiredIngrediant.totalMlOfMilk -= 75;
+                    RequiredIngrediant.totalGmOfCoffee -= 20;
+                    RequiredIngrediant.totalCups -= 1;
+                    RequiredIngrediant.totalMoney += 7;
+                    System.out.printf("I have enough resources, making you a coffee!\n");
+                } else {
+                    String shortResource = null;
+                    if (RequiredIngrediant.totalMlOfWater < 350) {
+                        shortResource = "water";
+                    } else if (RequiredIngrediant.totalMlOfMilk < 75) {
+                        shortResource = "milk";
+                    } else if (RequiredIngrediant.totalGmOfCoffee < 20) {
+                        shortResource = "coffee";
+                    } else if (RequiredIngrediant.totalCups < 1) {
+                        shortResource = "cups";
+                    }
+                    System.out.printf("Sorry, not enough %s!", shortResource);
+                }
                 break;
-            case 3 :
-                RequiredIngrediant.totalMlOfWater -= 200;
-                RequiredIngrediant.totalMlOfMilk -= 100;
-                RequiredIngrediant.totalGmOfCoffee -= 12;
-                RequiredIngrediant.totalCups -= 1;
-                RequiredIngrediant.totalMoney += 6;
+            case "3" :
+                if (RequiredIngrediant.totalMlOfWater >= 200 && RequiredIngrediant.totalMlOfMilk >= 100 && RequiredIngrediant.totalGmOfCoffee >= 12 && RequiredIngrediant.totalCups >= 1) {
+                    RequiredIngrediant.totalMlOfWater -= 200;
+                    RequiredIngrediant.totalMlOfMilk -= 100;
+                    RequiredIngrediant.totalGmOfCoffee -= 12;
+                    RequiredIngrediant.totalCups -= 1;
+                    RequiredIngrediant.totalMoney += 6;
+                    System.out.printf("I have enough resources, making you a coffee!\n");
+                } else {
+                    String shortResource = null;
+                    if (RequiredIngrediant.totalMlOfWater < 200) {
+                        shortResource = "water";
+                    } else if (RequiredIngrediant.totalMlOfMilk < 100) {
+                        shortResource = "milk";
+                    } else if (RequiredIngrediant.totalGmOfCoffee < 12) {
+                        shortResource = "coffee";
+                    } else if (RequiredIngrediant.totalCups < 1) {
+                        shortResource = "cups";
+                    }
+                    System.out.printf("Sorry, not enough %s!", shortResource);
+                }
+                break;
+            case "back" :
+                break;
         }
     }
 }
--- machine/CoffeeMachine.java
+++ machine/CoffeeMachine.java
@@ -3,10 +3,8 @@ package machine;
 public class CoffeeMachine {
     public static void main(String[] args) {
         RequiredIngrediant.setInitialCapicityOfMachine();
-        RequiredIngrediant.printIntialCapicityOfMachine();
         AskUser.writeAction();
         AskUser.perfromAction();
-        RequiredIngrediant.printIntialCapicityOfMachine();
 
     }
 }
